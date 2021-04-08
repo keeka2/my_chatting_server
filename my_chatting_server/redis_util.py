c96d8e1e5f87b1e4338b5e8a71e8b60a711d076a
nikolai <nikolai@buzzni.com>
2021-04-08 16:15:18 +0900

    Initial commit

---
+++ my_chatting_server/redis_util.py
@@ -0,0 +1,40 @@
+import redis
+
+
+class Singleton(type):
+    """
+    An metaclass for singleton purpose. Every singleton class should inherit from this class by 'metaclass=Singleton'.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class RedisClient(metaclass=Singleton):
+    HOST = "127.0.0.1"
+    PORT = 6379
+
+    def __init__(self):
+        self.pool = redis.ConnectionPool(
+            host=self.HOST, port=self.PORT
+        )
+
+    @property
+    def conn(self):
+        if not hasattr(self, "_conn"):
+            self.get_connection()
+        return self._conn
+
+    def get_connection(self):
+        self._conn = redis.Redis(connection_pool=self.pool)
+
+
+if __name__ == "__main__":
+    client = RedisClient()
+    client.conn.set('a', 'b')
+    redis_data = client.conn.get('a')
+    print(redis_data)
---
+++ tests/test_my_chatting_server.py
@@ -0,0 +1,5 @@
+from my_chatting_server import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
