846ff833af61d22826506ba522ac7a7749ac9aa4
nikolai <nikolai@buzzni.com>
2021-04-19 01:53:48 +0900

    서버 목록 가져오는 api

---
+++ api_server.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+from redis_database import get_server_list
+
+app = FastAPI()
+
+
+@app.get("/get/server_list/")
+def get_server():
+    resp = {"server_list": get_server_list()}
+    return resp
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
